d603feffde98433fdf5f76d954e42fa42d815c3e
Michael Guedko <mjgeko@gmail.com>
2022-04-27 12:43:39 -0400

    Add comments

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*
+Project properties:
+    title
+    duedate
+    description
+    dateCreated
+    todoList
+    checklist
+Todo properties:
+    title
+    description
+    dueDate
+    priority
+    dateCreated
+    notes
+    checklist
+*/
+/*
+    Functions:
+    view all projects/todos
+    create todo/project
+    expand todo to view/edit details(todo modal)
+    delete todo/project
+*/
