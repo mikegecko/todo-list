bd8262b9de17571c54be3346217fbeee6343f6ee
Michael Guedko <mjgeko@gmail.com>
2022-04-29 12:19:07 -0400

    Add comments

--- src/project.js
+++ src/project.js
@@ -1,7 +1,7 @@
 //This is a factory for creating/modifying/deleting projects
 
-const Project = function({name, index}){
+const Project = function({name, index, todoList}){
     this.name = name || "";
     this.index = index || 0;
-
+    this.todoList = todoList;
 }
--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,18 @@
-//Todo Factory
+// Todo factory
+/*
+                    Project-----------------------
+                        ||                      ||
+                    TodoList----------------   Name
+                        ||                ||      
+        [-----------TodoItem--------]  ...Todo Items     
+        |   |       |     |        |
+      name duedate notes priority isChecked
+*/  
+const TodoItem = ({name, dueDate, notes, priority, isChecked}) => {
+    this.name = name || "";
+    this.dueDate = dueDate || newDate(); //This probably wont work
+    this.notes = notes || "";
+    this.priority = priority || 0;
+    this.isChecked = isChecked || false;
+}
+
