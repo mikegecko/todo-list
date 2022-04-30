3d7d4e830b6a32e2b5d2ee43ec6909225170d91b
Michael Guedko <mjgeko@gmail.com>
2022-04-30 18:53:05 -0400

    Create todo & project methods

--- src/project.js
+++ src/project.js
@@ -1,7 +1,13 @@
 //This is a factory for creating/modifying/deleting projects
-
+const ProjectList = [];
 const Project = function({name, index, todoList}){
     this.name = name || "";
     this.index = index || 0;
-    this.todoList = todoList;
+    this.todoList = todoList || [];
+}
+//
+function createNewProject(name){
+    let index = ProjectList.length;
+    let newProject = new Project(name, index);
+    ProjectList.push(newProject);
 }
--- src/todo.js
+++ src/todo.js
@@ -2,17 +2,24 @@
 /*
                     Project-----------------------
                         ||                      ||
-                    TodoList----------------   Name
+                Array TodoList----------------   Name
                         ||                ||      
         [-----------TodoItem--------]  ...Todo Items     
         |   |       |     |        |
       name duedate notes priority isChecked
 */  
-const TodoItem = ({name, dueDate, notes, priority, isChecked}) => {
+const TodoItem = ({name, dueDate, notes, priority, isChecked, id}) => {
     this.name = name || "";
     this.dueDate = dueDate || newDate(); //This probably wont work
     this.notes = notes || "";
     this.priority = priority || 0;
     this.isChecked = isChecked || false;
+    this.id = id;
 }
-
+//This todo list needs to go under a Project object
+const todoList = [];
+//Create a TodoItem and add it to todoList under proper Project
+function addTodoItem (){
+    let newItem = new TodoItem();
+    todoList.push(newItem);
+}
