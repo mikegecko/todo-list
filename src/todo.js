e7d04d558fcd18a9114624118e247047378a9585
Michael Guedko <mjgeko@gmail.com>
2022-05-02 12:38:44 -0400

    Add removeTodoItem method

--- src/todo.js
+++ src/todo.js
@@ -18,6 +18,15 @@ function TodoList(name, index, todoList){
         let newTodoItem = new TodoItem(name, newID, dueDate, notes, priority, isChecked)
         this.todoList.push(newTodoItem);
     }
+    this.removeTodoItem = function (id){
+        this.todoList.splice(id,1);
+        this.updateId();
+    }
+    this.updateId = function(){
+        this.todoList.forEach(element => {
+            element.id = this.todoList.indexOf(element);
+        });
+    }
 }
 //TodoItem Constructor
 function TodoItem(name, id, dueDate, notes, priority, isChecked){
@@ -28,8 +37,13 @@ function TodoItem(name, id, dueDate, notes, priority, isChecked){
     this.isChecked = isChecked || false;
     this.id = id;
 }
+//Testing values & methods
 const defaultList = new TodoList("Default", 0);
 defaultList.addTodoItem("Clean Room",Date.now(),"Use vacuum", 1, false);
+defaultList.addTodoItem("Play games",Date.now(), "Minecraft", 2 ,false);
+defaultList.addTodoItem("Play games",Date.now(), "Minecraft", 2 ,false);
+defaultList.addTodoItem("Play games",Date.now(), "Minecraft", 2 ,false);
+defaultList.removeTodoItem(2);
 console.table(defaultList.todoList);
 
 
